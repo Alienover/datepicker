84e15573af818a176ca46334be6750d9dfd2e178
Alienvover <517854809@qq.com>
2017-12-13 12:49:28 +0800

    增加了调用时的返回对象,让使用者可以获取当前选择的日期

--- README.md
+++ README.md
@@ -1,16 +1,46 @@
-# datepicker
+# Datepicker
 
-### 使用
-`HTML`中需要添加一个类名为`datepicker`的容器,并引入相应的`CSS`样式表和`JS`文件
-```html
-  <link rel="stylesheet" href="/css/datepicker.css">
-  <div class="datepicker"></div>
-  <script type="text/javascript" src="datepicker.js"></script>
+> 一款基于原生JS的简易日期选择器
+
+项目演示: [DEMO](https://alienover.github.io/datepicker/)
+
+### Usage
+只需执行`datepicker()`函数即可以默认的形式初始化改日历,若需要指定特定的时间,则可以传入一个对象,其中包含主键为`date`,其值的格式为`YY-MM-DD`,`datepicker`渲染完成之后,返回该组件的`DOM`对象以及[相应的方法](#method)
+``` javascript
+  var date = datepicker({
+    //选填,指定要生成的datepicker的容器,默认自动为ClassName = datepicker的容器
+    ele: ".datepicker",
+    //选填,指定日期,可指定年月,也可年月日,默认为系统当前时间
+    date: "2017-11-11",
+  });
 ```
 
-只需执行`datepicker.init()`函数即可以默认的形式初始化改日历,若需要指定特定的时间,则可以传入一个对象,其中包含主键为`date`,其值的格式为`YY-MM-DD`
+### Method
+
+``` html
+  <link rel="stylesheet" href="/css/datepicker.css">
+```
+
+#### date.gePickedDate(symbol)
 ``` javascript
-  datepicker.init({
-    date: "2017-11-11",
-  })
+  /**
+    * @desc 用于获取当前被选中的日期
+    * @param {String} symbol 指定日期的连接符,默认为'-'
+    * @return {Object}
+    */
+```
+
+使用指定的`symbol`,如果`symbol`为空,则使用`'-'`,作为连接符号,将当前选择的日期拼接起来,返回`{String}`类型,若未选择,则返回默认时间
+
+``` javascript
+  var date = datepicker({
+    ele: '.date',
+    date: '2017-12-12',
+  });
+
+  console.log(date.getPickedDate());
+  //"2017-12-12"
+
+  console.log(date.getPickedDate('/'));
+  //"2017/12/12"
 ```
--- datepicker.js
+++ datepicker.js
@@ -1,6 +1,16 @@
-(function () {
+;(function () {
+  //为了避免污染全局对象,使用匿名函数执行
+
   var datepicker = {};
   var ele = {};
+
+  /**
+    * @desc 生成指定日期的当月数据
+    * @param {Number} year 指定年份,选填
+    * @param {Number} month 指定月份,选填
+    * @param {Number} thisDate 指定日期,选填
+    * @return {Array}
+    */
 
   datepicker.getMonthData = function(year, month, thisDate) {
 
@@ -17,16 +27,16 @@
     ret.thisMonth = month;
     ret.thisDate = (!thisDate) ? 'undefined' : today.getDate();
 
-    var firstDate = new Date(year, month - 1, 1);
-    var firstDateOfDay = firstDate.getDay();
+    var firstDate = new Date(year, month - 1, 1),
+        firstDateOfDay = firstDate.getDay(),
 
-    var lastDate = new Date(year, month, 0);
-    var lastDateOfDay = lastDate.getDay();
+        lastDate = new Date(year, month, 0),
+        lastDateOfDay = lastDate.getDay(),
 
-    var daysCount = lastDate.getDate();
+        daysCount = lastDate.getDate(),
 
-    var head = - firstDateOfDay + 1;
-    var end = daysCount + (6 - lastDateOfDay);
+        head = - firstDateOfDay + 1,
+        end = daysCount + (6 - lastDateOfDay);
 
     for (var i = head; i <= end; i++) {
       var thisMonth = month - 1;
@@ -42,18 +52,30 @@
   };
 
   datepicker.ui =  {
+    /**
+      * @desc 初始化UI
+      * @param {Number} year 指定年份,选填
+      * @param {Number} month 指定月份,选填
+      * @param {Number} date 指定日期,选填
+      */
+
     init: function (year, month, date) {
       this.build();
       this.getDate(year, month, date);
     },
+
+    /**
+      * @desc 生成datepicker的基本UI,即年月显示,日期的表头
+      */
+
     build: function() {
       var $ = ele;
       var initial = '';
       initial +=   '<div class="ui-datepicker-wrapper">';
       initial +=     '<div class="ui-datepicker-header">';
-      initial +=       '<a data-rel="prev" class="ui-datepicker-btn ui-datepicker-prev-btn">&lt;</a>';
+      initial +=       '<a href="javascript:void(0);" data-rel="prev" class="ui-datepicker-btn ui-datepicker-prev-btn">&lt;</a>';
       initial +=       '<span class="ui-datepicker-display"><a class="ui-datepicker-display-year"></a> - <a class="ui-datepicker-display-month"></a></span>';
-      initial +=       '<a data-rel="next" class="ui-datepicker-btn ui-datepicker-next-btn">&gt;</a>';
+      initial +=       '<a href="javascript:void(0);" data-rel="next" class="ui-datepicker-btn ui-datepicker-next-btn">&gt;</a>';
       initial +=     '</div>';
       initial +=     '<div class="ui-datepicker-body">';
       initial +=        '<table>';
@@ -76,17 +98,28 @@
 
       $.innerHTML = initial;
     },
-    getDate: function (year, month, date) {
-      var $ = ele;
-      var monthData = datepicker.getMonthData(year, month, date);
-      var thisDate = monthData.thisDate;
 
-      var tbody = '';
+    /**
+      * @desc 根据指定日期,获取当月数据
+      * @param {Number} year 指定年份,选填
+      * @param {Number} month 指定月份,选填
+      * @param {Number} date 指定日期,选填
+      */
+
+    getDate: function (year, month, date) {
+      var $ = ele,
+          monthData = datepicker.getMonthData(year, month, date),
+          thisDate = monthData.thisDate,
+
+          tbody = '';
       for (var i = 0; i < monthData.data.length; i++) {
         var data = monthData.data[i];
         if ((i % 7) === 0) {
           tbody += '<tr>';
         }
+        //通过两个判断
+        //Boolean(data.order) 判断当前渲染的日期,是否为当月日期,ture为当月日期,false为上个月的或下个月的日期
+        //(data.showDate == thisDate) 判断当前渲染的日期,是否为指定的日期,若无指定日期,则thisDate为undefined
         tbody += '<td class="' + ((Boolean(data.order)) ? ((data.showDate == thisDate) ? 'active' : '') : 'notCurrMonth ') + '" data-rel=\'' + data.date + '\'>' + data.showDate + '</td>';
         if ((i % 7) === 6) {
           tbody += '</tr>';
@@ -99,12 +132,20 @@
     },
   };
 
+  /**
+    * @desc 为datepicker区域内的绑定点击事件
+    * 通过switch case 来判断当前点击的是哪一块
+    * /prev/.test(rel) 判断是否点击上一个月按钮
+    * /next/.test(rel) 判断是否点击下一个月按钮
+    * (/[0-9]/i.test(rel)) 判断是否点击日期
+    */
   datepicker.bindEvents = function () {
-    var wrapper = document.querySelector('.ui-datepicker-wrapper');
+    var wrapper = ele.querySelector('.ui-datepicker-wrapper');
     wrapper.addEventListener('click', function (e) {
-      var currYear = Number(wrapper.querySelector('.ui-datepicker-display-year').text);
-      var currMonth = Number(wrapper.querySelector('.ui-datepicker-display-month').text);
-      var rel = e.target.dataset.rel;
+      var currYear = Number(wrapper.querySelector('.ui-datepicker-display-year').text),
+          currMonth = Number(wrapper.querySelector('.ui-datepicker-display-month').text),
+          rel = e.target.dataset.rel;
+      ele = this.parentNode;
       switch (true) {
         case /prev/.test(rel):
           datepicker.ui.getDate(currYear, (currMonth - 1));
@@ -116,19 +157,54 @@
           datepicker.ui.getDate(currYear, currMonth, rel);
           break;
         default:
-          console.log(/[0-9]+/.test(rel));
           break;
       }
     });
   };
 
-  datepicker.init = function (object) {
-    ele = object.ele || '.datepicker';
-    ele = document.querySelector(ele);
-    var date = (object.date) ? object.date.split('-') : "";
-    this.ui.init(date[0], date[1], date[2]);
-    this.bindEvents();
+  /**
+    * @desc 用于获取当前被选中的日期
+    * @param {String} symbol 指定日期的连接符,默认为'-'
+    * @return {Object}
+    */
+  datepicker.getPickedDate = function (symbol) {
+    var _this = this.ele;
+    symbol = symbol || '-';
+    var currYear = Number(_this.querySelector('.ui-datepicker-display-year').text),
+        currMonth = Number(_this.querySelector('.ui-datepicker-display-month').text),
+        currDate = Number(_this.querySelector('.ui-datepicker-body .active').innerHTML);
+    return {
+      "currYear:":currYear,
+      "currMonth:":currMonth,
+      "currDate:":currDate,
+    };
   };
 
-  window.datepicker = datepicker;
+  /**
+    * @desc datepicker 的初始化函数
+    * @param {Object} configure 初始化时的配置对象,其中包括dom对象"ele",指定日期"date"
+    * @return {Object} 返回操作当前对象以及相应的方法
+    */
+  datepicker.init = function (configure) {
+    configure = configure || {};
+    ele = (typeof configure.ele !== 'undefined') ? (configure.ele) : '.datepicker';
+    ele = document.querySelector(ele);
+    var date = (configure.date) ? configure.date.split('-') : new Date().toLocaleDateString().split('/');
+    try {
+      datepicker.ui.init(date[0], date[1], date[2]);
+      datepicker.bindEvents();
+
+      return {
+        "ele": ele,
+        "getPickedDate": datepicker.getPickedDate,
+      };
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  /**
+    * @desc 将datepicker的init函数,getPickedDate函数暴露给全局对象,以便使用者使用
+    */
+  window.datepicker = datepicker.init;
 })();
